cdba0edb075547429ca7ea356aa52d4495095698
yangwen <yangwen@thoughtworks.ocm>
2018-03-31 08:02:11 +0800

    include a failing test

---
+++ src/test/java/cn/thoughtworks/school/core/StudentDomainServiceTest.java
@@ -0,0 +1,13 @@
+package cn.thoughtworks.school.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StudentDomainServiceTest {
+
+    @Test
+    public void shouldNeverEverPassUnlessYouChangeIt() {
+        assertTrue(false);
+    }
+}
