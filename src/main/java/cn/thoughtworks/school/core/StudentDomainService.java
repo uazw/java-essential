dd56e0a6935e7e21a4357ad963e104b9ee1bc57f
yangwen <yangwen@thoughtworks.ocm>
2018-04-01 10:51:48 +0800

    update answer.

--- src/main/java/cn/thoughtworks/school/core/StudentDomainService.java
+++ src/main/java/cn/thoughtworks/school/core/StudentDomainService.java
@@ -1,10 +1,18 @@
 package cn.thoughtworks.school.core;
 
+import java.util.Comparator;
 import java.util.List;
+
+import static java.lang.String.format;
 
 public class StudentDomainService {
 
     public void printAllStudentInformation(List<Student> students) {
-        students.forEach(System.out::println);
+        students
+                .stream()
+                .sorted(Comparator.comparing(Student::getScore).reversed())
+                .limit(50)
+                .map(student -> format("%s, %s", student.getName(), student.getScore()))
+                .forEach(System.out::println);
     }
 }
